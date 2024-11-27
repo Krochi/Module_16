d6822c3869eb85f2e91864803147cc1cb6a51d85
Krochi <77779207+Krochi@users.noreply.github.com>
2024-11-27 06:12:49 +0300

    Add files via upload

--- module_16_3/main.py
+++ module_16_3/main.py
@@ -10,24 +10,24 @@ class User(BaseModel):
     age: int
 
 @app.get("/user")
-def get_user():
+async def get_user():
     return user
 
-@app.post("/user{username}/{age}")
-def post_user(username: str, age: int):
+@app.post("/user/{username}/{age}")
+async def post_user(username: str, age: int):
     user_id = str(max(map(int, user.keys())) + 1)
     user[user_id] = f'Имя:{username}, возраст:{age}'
     return f'User {user_id} is registered'
 
-@app.put("/user{user_id}/{username}/{age}")
-def put_user(user_id: int, username: str, age: int):
+@app.put("/user/{user_id}/{username}/{age}")
+async def put_user(user_id: str, username: str, age: int):
     if user_id in user:
         user[user_id] = f'Имя: {username}, возраст: {age}'
         return f'User {user_id} has been updated'
     return f'User {user_id} not found'
 
-@app.delete("/user{user_id}")
-def delete_user(user_id: int):
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: str):
     if user_id in user:
         del user[user_id]
         return f'User {user_id} has been deleted'
